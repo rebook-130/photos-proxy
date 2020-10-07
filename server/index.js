562239e40fc4fefa8145b7757c4d2c70b14f3436
Catherine Straus <catherinestraus@berkeley.edu>
2020-10-07 16:49:10 -0700

    Completed proxy

--- server/index.js
+++ server/index.js
@@ -19,5 +19,9 @@ app.use('/api/calendar', createProxyMiddleware({ target: `http://localhost:3002`
 app.use('/api/rooms/:roomId', createProxyMiddleware({ target: `http://localhost:3003`, changeOrigin: true }));
 
 // More Places to Stay
-app.use('/api/more_places', createProxyMiddleware({ target: `http://localhost:3004`, changeOrigin: true }));
-app.use('/api/saved_lists', createProxyMiddleware({ target: `http://localhost:3004`, changeOrigin: true }));
+app.use('/api/more_places', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
+app.use('/api/saved_lists', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
+app.use('/api/create_list', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
+app.use('/api/update_listing', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
+app.use('/api/update_collection', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
+app.use('/api/collection_name', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
