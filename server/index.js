41f0c7a23b7dabcf8f7ca174d8f310d30562afbb
Catherine Straus <catherinestraus@berkeley.edu>
2020-10-10 00:00:44 -0700

    Remove the duplicate http://

--- server/index.js
+++ server/index.js
@@ -16,12 +16,12 @@ app.use('/api/photogallery/:roomId', createProxyMiddleware({ target: `http://54.
 app.use('/api/calendar', createProxyMiddleware({ target: `http://calendar-service:3002`, changeOrigin: true }));
 
 // Reviews
-app.use('/api/rooms/:roomId', createProxyMiddleware({ target: `http://http://3.136.76.170:3003`, changeOrigin: true }));
+app.use('/api/rooms/:roomId', createProxyMiddleware({ target: `http://3.136.76.170:3003`, changeOrigin: true }));
 
 // More Places to Stay
-app.use('/api/more_places', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
-app.use('/api/saved_lists', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
-app.use('/api/create_list', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
-app.use('/api/update_listing', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
-app.use('/api/update_collection', createProxyMiddleware({ target: `http://http://54.219.211.68/:3004/`, changeOrigin: true}));
-app.use('/api/collection_name', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
+app.use('/api/more_places', createProxyMiddleware({ target: `http://54.219.211.68:3004/`, changeOrigin: true}));
+app.use('/api/saved_lists', createProxyMiddleware({ target: `http://54.219.211.68:3004/`, changeOrigin: true}));
+app.use('/api/create_list', createProxyMiddleware({ target: `http://54.219.211.68:3004/`, changeOrigin: true}));
+app.use('/api/update_listing', createProxyMiddleware({ target: `http://54.219.211.68:3004/`, changeOrigin: true}));
+app.use('/api/update_collection', createProxyMiddleware({ target: `http://54.219.211.68/:3004/`, changeOrigin: true}));
+app.use('/api/collection_name', createProxyMiddleware({ target: `http://54.219.211.68:3004/`, changeOrigin: true}));
