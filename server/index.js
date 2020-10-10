8605a9fb2821f723b20a1ddd74ae3df9cee8b4b8
Catherine Straus <catherinestraus@berkeley.edu>
2020-10-09 20:19:12 -0700

    Add in changes to proxy to support containers

--- server/index.js
+++ server/index.js
@@ -10,18 +10,18 @@ app.listen( port, () => {
 })
 
 // Photo-gallery
-app.use('/api/photogallery/:roomId', createProxyMiddleware({ target: `http://localhost:3001/`, changeOrigin: true }));
+app.use('/api/photogallery/:roomId', createProxyMiddleware({ target: `http://photo-gallery-service:3001/`, changeOrigin: true }));
 
 // Calender
-app.use('/api/calendar', createProxyMiddleware({ target: `http://localhost:3002`, changeOrigin: true }));
+app.use('/api/calendar', createProxyMiddleware({ target: `http://calendar-service:3002`, changeOrigin: true }));
 
 // Reviews
-app.use('/api/rooms/:roomId', createProxyMiddleware({ target: `http://localhost:3003`, changeOrigin: true }));
+app.use('/api/rooms/:roomId', createProxyMiddleware({ target: `http://review-service:3003`, changeOrigin: true }));
 
 // More Places to Stay
-app.use('/api/more_places', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
-app.use('/api/saved_lists', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
-app.use('/api/create_list', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
-app.use('/api/update_listing', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
-app.use('/api/update_collection', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
-app.use('/api/collection_name', createProxyMiddleware({ target: `http://localhost:3004/`, changeOrigin: true}));
+app.use('/api/more_places', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
+app.use('/api/saved_lists', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
+app.use('/api/create_list', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
+app.use('/api/update_listing', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
+app.use('/api/update_collection', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
+app.use('/api/collection_name', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
