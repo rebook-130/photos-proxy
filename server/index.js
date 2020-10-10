7a0cb978fc34fc698fb34a8f94ae3edcd5da9ef9
Catherine Straus <catherinestraus@berkeley.edu>
2020-10-09 23:45:39 -0700

    Added each component's elastic IP

--- server/index.js
+++ server/index.js
@@ -10,18 +10,18 @@ app.listen( port, () => {
 })
 
 // Photo-gallery
-app.use('/api/photogallery/:roomId', createProxyMiddleware({ target: `http://photo-gallery-service:3001/`, changeOrigin: true }));
+app.use('/api/photogallery/:roomId', createProxyMiddleware({ target: `http://54.219.99.93:3001/`, changeOrigin: true }));
 
 // Calender
 app.use('/api/calendar', createProxyMiddleware({ target: `http://calendar-service:3002`, changeOrigin: true }));
 
 // Reviews
-app.use('/api/rooms/:roomId', createProxyMiddleware({ target: `http://review-service:3003`, changeOrigin: true }));
+app.use('/api/rooms/:roomId', createProxyMiddleware({ target: `http://http://3.136.76.170:3003`, changeOrigin: true }));
 
 // More Places to Stay
-app.use('/api/more_places', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
-app.use('/api/saved_lists', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
-app.use('/api/create_list', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
-app.use('/api/update_listing', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
-app.use('/api/update_collection', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
-app.use('/api/collection_name', createProxyMiddleware({ target: `http://more-places-to-stay-service:3004/`, changeOrigin: true}));
+app.use('/api/more_places', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
+app.use('/api/saved_lists', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
+app.use('/api/create_list', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
+app.use('/api/update_listing', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
+app.use('/api/update_collection', createProxyMiddleware({ target: `http://http://54.219.211.68/:3004/`, changeOrigin: true}));
+app.use('/api/collection_name', createProxyMiddleware({ target: `http://http://54.219.211.68:3004/`, changeOrigin: true}));
